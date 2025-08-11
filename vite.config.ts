893d0dea7eb2f6cd27175a57a19255e2bb9f76ed
Shubham Sharma <shubhamoksharma@gmail.com>
2025-08-11 21:03:43 +0530

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/bhola-shankar-glow-up/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    react()
+  ],
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
